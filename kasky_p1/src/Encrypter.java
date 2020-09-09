d6806035c3ede1a7365720d9ef2e7768f4b3603e
Evan Kasky <ekasky25@gmail.com>
2020-09-09 19:15:03 -0400

    [PA #1]: Adds Encrypter class

--- kasky_p1/src/Encrypter.java
+++ kasky_p1/src/Encrypter.java
@@ -1,5 +1,39 @@
 public class Encrypter {
 
+    public static void main(String[] args) {
 
+        Encrypter test = new Encrypter();
+        String encrypted = test.encrypt("1234");
+        System.out.print(encrypted);
+
+    }
+
+    public String encrypt(String digits) {
+
+        String encrypted = "";
+        int digitArray[] = new int[4];
+        int temp;
+
+        for(int i = 0; i < 4; i++) {
+            digitArray[i] = Character.getNumericValue(digits.charAt(i));
+            digitArray[i] +=7;
+            digitArray[i] = digitArray[i] % 10;
+        }
+
+        temp = digitArray[0];
+        digitArray[0] = digitArray[2];
+        digitArray[2] = temp;
+
+        temp = digitArray[1];
+        digitArray[1] = digitArray[3];
+        digitArray[3] = temp;
+
+        for(int i = 0; i < 4; i++) {
+            encrypted = encrypted.concat(Integer.toString(digitArray[i]));
+        }
+
+        return encrypted;
+
+    }
 
 }
