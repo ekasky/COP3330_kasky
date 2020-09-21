58482dac98e0a5e57c89a2f8177cc352306eceb2
Evan Kasky <ekasky25@gmail.com>
2020-09-21 17:39:51 -0400

    [PA: #2]: Inital Commit for Programming Assignment 2

---
+++ kasky_p2/src/App.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class App {
+
+    public static void main(String[] args) {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
+
+}
