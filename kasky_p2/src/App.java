32fe342df2c116f323b9c2a6a0ecbdfba0d2368c
Evan Kasky <ekasky25@gmail.com>
2020-09-26 15:26:27 -0400

    [PA #2]: Reset Assignemnt 2 Code

--- kasky_p2/src/App.java
+++ kasky_p2/src/App.java
@@ -1,21 +1,3 @@
-import java.util.ArrayList;
-
 public class App {
-
-    public static void main(String[] args) {
-        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
-
-        while (moreInput()) {
-            double height = getUserHeight();
-            double weight = getUserWeight();
-
-            BodyMassIndex bmi = new BodyMassIndex(height, weight);
-            bmiData.add(bmi);
-
-            displayBmiInfo(bmi);
-        }
-
-        displayBmiStatistics(bmiData);
-    }
 
 }
