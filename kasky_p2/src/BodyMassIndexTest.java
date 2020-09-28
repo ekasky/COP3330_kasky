019e60a8fc718439010df4386a5a934f3f667fc1
Evan Kasky <ekasky25@gmail.com>
2020-09-28 13:06:06 -0400

    [PA #2]: Adds Test cases

--- kasky_p2/src/BodyMassIndexTest.java
+++ kasky_p2/src/BodyMassIndexTest.java
@@ -6,20 +6,96 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BodyMassIndexTest {
 
+    //Under weight
+    private BodyMassIndex underWeight = new BodyMassIndex(78, 150);
+
+    //Normal weight
+    private BodyMassIndex normalWeight = new BodyMassIndex(65, 130);
+
+    //Over weight
+    private BodyMassIndex overWeight = new BodyMassIndex(56, 125);
+
+    //Obese
+    private BodyMassIndex obese = new BodyMassIndex(66, 300);
 
     @Test
     void getBmi() {
+
+        double expectedBmi = 21.6;
+        assertEquals(expectedBmi, normalWeight.getBmi());
+
     }
 
     @Test
     void getCat() {
+
+        String expectedCat = "Normal Weight";
+        assertEquals(expectedCat, normalWeight.getCat());
+
     }
 
     @Test
-    void calcBmi() {
+    void calcBmiUnderWeight() {
+
+        double expectedBmi = 17.3;
+        assertEquals(expectedBmi, underWeight.getBmi());
+
     }
 
     @Test
-    void category() {
+    void categoryUnderWeight() {
+
+        String expectedCat = "Under Weight";
+        assertEquals(expectedCat, underWeight.getCat());
+
     }
+
+    @Test
+    void calcBmiNormalWeight() {
+
+        double expectedBmi = 21.6;
+        assertEquals(expectedBmi, normalWeight.getBmi());
+
+    }
+
+    @Test
+    void categoryNormalWeight() {
+
+        String expectedCat = "Normal Weight";
+        assertEquals(expectedCat, normalWeight.getCat());
+
+    }
+
+    @Test
+    void calcBmiOverWeight() {
+
+        double expectedBmi = 28.0;
+        assertEquals(expectedBmi, overWeight.getBmi());
+
+    }
+
+    @Test
+    void categoryOverWeight() {
+
+        String expectedCat = "Over Weight";
+        assertEquals(expectedCat, overWeight.getCat());
+
+    }
+
+    @Test
+    void calcBmiObese() {
+
+        double expectedBmi = 48.4;
+        assertEquals(expectedBmi, obese.getBmi());
+
+    }
+
+    @Test
+    void categoryObese() {
+
+        String expectedCat = "Obese";
+        assertEquals(expectedCat, obese.getCat());
+
+    }
+
 }
