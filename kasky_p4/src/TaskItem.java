47576ac10eedbefe74f3166fb57d9996ffdd5932
Evan Kasky <ekasky25@gmail.com>
2020-11-14 13:59:51 -0500

    [PA: #4]: Adds Constructor for TaskItem

--- kasky_p4/src/TaskItem.java
+++ kasky_p4/src/TaskItem.java
@@ -4,4 +4,10 @@ public class TaskItem {
     private String description;
     private String dueDate;
 
+    TaskItem(String title, String description, String dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
 }
