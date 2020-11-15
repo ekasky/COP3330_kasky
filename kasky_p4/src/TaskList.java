fb592dfdda5ff3a6baf1cf8aa046c0658d3815c6
Evan Kasky <ekasky25@gmail.com>
2020-11-15 18:13:06 -0500

    [PA: #4]: Adds addTask Method getTask Method and getTitle Method

--- kasky_p4/src/TaskList.java
+++ kasky_p4/src/TaskList.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TaskList {
 
-    
+    static Scanner input = new Scanner(System.in);
+    private ArrayList<TaskItem> list = new ArrayList<>();
+
+     public void addTask() {
+
+        TaskItem item = new TaskItem();
+
+        item.setTitle(getTitle());
+        item.setDescription(getDescription());
+        
+
+     }
+
+     private String getTitle() {
+
+         String title = null;
+
+         System.out.print("Task Title: ");
+
+         title = input.nextLine();
+
+         return title;
+
+
+
+     }
+
+     private String getDescription() {
+
+         String description = null;
+
+         System.out.print("Task Description: ");
+
+         description = input.nextLine();
+
+         return description;
+
+     }
 
 }
