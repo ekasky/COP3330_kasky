9b640279bcc3eef9d769576becd2f04e54977328
Evan Kasky <ekasky25@gmail.com>
2020-11-15 16:51:50 -0500

    [PA: #4]: Adds title getter and setter for TaskItem

--- kasky_p4/src/TaskItem.java
+++ kasky_p4/src/TaskItem.java
@@ -1,4 +1,14 @@
 public class TaskItem {
 
+    private String title;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 
 }
