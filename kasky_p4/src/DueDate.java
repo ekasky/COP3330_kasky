8f8e3e7fadce47dbb9ed1fee6c0fb7fb8368dc08
Evan Kasky <ekasky25@gmail.com>
2020-11-15 11:50:43 -0500

    [PA: #4]: Adds getMonth method

--- kasky_p4/src/DueDate.java
+++ kasky_p4/src/DueDate.java
@@ -29,4 +29,29 @@ public class DueDate {
 
     }
 
+    public int getMonth() {
+
+        int month;
+
+        do {
+
+            System.out.print("Enter a month with format [mm]: ");
+
+            while(!input.hasNextInt()) {
+                System.out.print("Error...Not a valid month...Try again: ");
+                input.next();
+            }
+
+            month = input.nextInt();
+
+            if(month < 0 || month > 12) {
+                System.out.println("Error...Not a valid month...Try again");
+            }
+
+        } while(month < 1 || month > 12);
+
+        return month;
+
+    }
+
 }
