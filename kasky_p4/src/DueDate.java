434df7d96ec0ceac560c368ddd765f192d3b342b
Evan Kasky <ekasky25@gmail.com>
2020-11-15 11:53:48 -0500

    [PA: #4]: Adds leapYear method

--- kasky_p4/src/DueDate.java
+++ kasky_p4/src/DueDate.java
@@ -54,4 +54,22 @@ public class DueDate {
 
     }
 
+    public static boolean leapYear(int year) {
+
+        boolean leap = false;
+
+        if(year % 400 == 0) {
+            leap = true;
+        }
+        else if(year % 4 == 0 && year % 100 != 0) {
+            leap = true;
+        }
+        else {
+            leap = false;
+        }
+
+        return leap;
+
+    }
+
 }
