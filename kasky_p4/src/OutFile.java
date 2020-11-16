4d594386ee56e6e5f7cfd6050f897e85a02120ee
Evan Kasky <ekasky25@gmail.com>
2020-11-15 21:23:38 -0500

    [PA: #4]: Adds methods to get save path

--- kasky_p4/src/OutFile.java
+++ kasky_p4/src/OutFile.java
@@ -1,7 +1,25 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class OutFile {
 
-    private File fileName = new File("")
+    static Scanner input = new Scanner(System.in);
+    private String fileName;
+    private String path;
+
+    public void getFileName() {
+        System.out.print("Enter the filename to save as: ");
+        this.fileName = input.nextLine();
+    }
+
+    public void getPath() {
+
+        getFileName();
+        this.path = "/kasky_p4/Lists/" + fileName;
+
+    }
+
+    
+
 
 }
