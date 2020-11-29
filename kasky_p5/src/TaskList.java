bf8a0f41ccd5511b312972c38ff8fe5104bedca5
Evan Kasky <ekasky25@gmail.com>
2020-11-29 11:45:24 -0500

    [PA: #5]: Adds method to print complete tasks

--- kasky_p5/src/TaskList.java
+++ kasky_p5/src/TaskList.java
@@ -30,4 +30,16 @@ public class TaskList {
 
     }
 
+    public void printComplete() {
+
+        System.out.println("Complete Tasks");
+        System.out.println("--------------");
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).isComplete())
+                System.out.println(i + ") " + list.get(i).toString());
+        }
+        System.out.println();
+
+    }
+
 }
