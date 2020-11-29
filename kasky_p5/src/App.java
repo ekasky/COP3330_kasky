e38e618cecb88f8e8a3afa50e0c384db123a7065
Evan Kasky <ekasky25@gmail.com>
2020-11-29 12:17:54 -0500

    [PA: #5]: Adds method to obtain a integer from user

--- kasky_p5/src/App.java
+++ kasky_p5/src/App.java
@@ -1,7 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
+
+
+    }
+
+    public static int getInteger() {
+
+        int integer;
+
+        try {
+            integer = input.nextInt();
+        }
+        catch (InputMismatchException e) {
+
+            input.next();
+            return -1;
+
+        }
+
+        return integer;
+
     }
 
 }
