e7f2ef3d0154b83df44f8e8a9016636df7b3d38e
Evan Kasky <ekasky25@gmail.com>
2020-11-29 11:37:44 -0500

    [PA: #5]: Adds method to add to list

--- kasky_p5/src/TaskList.java
+++ kasky_p5/src/TaskList.java
@@ -4,4 +4,19 @@ public class TaskList {
 
     private ArrayList<TaskItem> list = new ArrayList<>();
 
+    public boolean addTask(String title, String description, int year, int month, int day, boolean complete) {
+
+        TaskItem item = new TaskItem();
+        boolean val = item.createItem(title, description, year, month, day, complete);
+
+        if(val) {
+            list.add(item);
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
 }
