8cc758cdcaf55957817afdabd6a7fc3e66ea0ec2
Evan Kasky <ekasky25@gmail.com>
2020-12-01 12:12:08 -0500

    [PA: #5]: Adds getters for contact items

---
+++ kasky_p5/src/ContactItem.java
@@ -0,0 +1,24 @@
+public class ContactItem {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
