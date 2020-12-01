0e0bb83bcb3460123c911c8b1f8927c4fd3a4136
Evan Kasky <ekasky25@gmail.com>
2020-12-01 09:45:04 -0500

    [PA: #5]: Adds method to print list

--- kasky_p5/src/TaskList.java
+++ kasky_p5/src/TaskList.java
@@ -116,4 +116,14 @@ public class TaskList {
 
     }
 
+    private void printList() {
+
+        System.out.println("Current List");
+        System.out.println("------------");
+        for(int i = 0; i < list.size(); i++)
+            System.out.println(i + ") " + list.get(i).toString());
+        System.out.println();
+
+    }
+
 }
