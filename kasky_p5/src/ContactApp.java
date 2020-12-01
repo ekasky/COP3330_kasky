4dc81b0597f4c36af33d246b2146676b83bff280
Evan Kasky <ekasky25@gmail.com>
2020-12-01 12:37:50 -0500

    [PA: #5]: Adds contactMenu method

--- kasky_p5/src/ContactApp.java
+++ kasky_p5/src/ContactApp.java
@@ -11,4 +11,18 @@ public class ContactApp {
 
     }
 
+    private static void contactMenu() {
+
+        System.out.println("List Operation Menu");
+        System.out.println("-------------------");
+        System.out.println("1. View the list");
+        System.out.println("2. Add an item");
+        System.out.println("3. Edit an item");
+        System.out.println("4. Remove a item");
+        System.out.println("5. Save the current list");
+        System.out.println("6. Quit to main menu");
+        System.out.print("> ");
+
+    }
+
 }
