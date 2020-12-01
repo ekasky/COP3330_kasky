aa0003bcfeecae45d488abbdbff72368fcb613d5
Evan Kasky <ekasky25@gmail.com>
2020-12-01 08:52:06 -0500

    [PA: #5]: Adds getInteger method

--- kasky_p5/src/App.java
+++ kasky_p5/src/App.java
@@ -1,16 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+
+
     }
 
-    public static void applicationMenu() {
+    private static void applicationMenu() {
         System.out.println("Application Menu");
         System.out.println("----------------");
         System.out.println("1. Task List");
         System.out.println("2. Contact List");
         System.out.println("3. Quit");
         System.out.print("> ");
+    }
+
+
+
+    public static int getInteger() {
+
+        int integer;
+
+        try {
+            integer = input.nextInt();
+        }
+        catch (InputMismatchException e) {
+            input.next();
+            return -1;
+        }
+
+        return integer;
+
     }
 
 }
