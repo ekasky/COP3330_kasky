a2eae3c317096466f5e0c9c3c48f014b00832888
Evan Kasky <ekasky25@gmail.com>
2020-12-01 09:00:01 -0500

    [PA: #5]: Adds task list main menu

---
+++ kasky_p5/src/TaskList.java
@@ -0,0 +1,13 @@
+public class TaskList {
+
+    private static void mainMenu() {
+        System.out.println("Main Menu");
+        System.out.println("---------");
+        System.out.println("1. Create a new list");
+        System.out.println("2. Load a existing list");
+        System.out.println("3. Quit");
+        System.out.print("> ");
+    }
+    
+
+}
