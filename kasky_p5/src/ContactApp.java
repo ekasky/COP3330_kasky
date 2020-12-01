a789976946161d1d3e123e680d25ebc2e6edfbec
Evan Kasky <ekasky25@gmail.com>
2020-12-01 12:35:47 -0500

    [PA: #5]: Adds mainMenu method for contact list

---
+++ kasky_p5/src/ContactApp.java
@@ -0,0 +1,14 @@
+public class ContactApp {
+
+    private static void mainMenu() {
+
+        System.out.println("Main Menu");
+        System.out.println("---------");
+        System.out.println("1. Create a new list");
+        System.out.println("2. Load an existing list");
+        System.out.println("3. Quit");
+        System.out.print("> ");
+
+    }
+
+}
