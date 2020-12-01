2402449efdc9db09537ef2663d0878de82855a64
Evan Kasky <ekasky25@gmail.com>
2020-12-01 08:49:39 -0500

    [PA: #5]: Adds applicationMenu

---
+++ kasky_p5/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void applicationMenu() {
+        System.out.println("Application Menu");
+        System.out.println("----------------");
+        System.out.println("1. Task List");
+        System.out.println("2. Contact List");
+        System.out.println("3. Quit");
+        System.out.print("> ");
+    }
+
+}
