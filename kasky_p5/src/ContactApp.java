76ef56f2878fc7d7df56db5ab0b2c02e54b7c37d
Evan Kasky <ekasky25@gmail.com>
2020-12-01 12:41:05 -0500

    [PA: #5]: Adds contactLoop method

--- kasky_p5/src/ContactApp.java
+++ kasky_p5/src/ContactApp.java
@@ -25,4 +25,49 @@ public class ContactApp {
 
     }
 
+    public void contactLoop() {
+
+        int run;
+
+        do {
+
+            contactMenu();
+            run = App.getInteger();
+
+            switch (run) {
+
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                default:
+                    System.out.println("\nWARNING: NOT A VALID CHOICE\n");
+                    break;
+
+            }
+
+        } while(run != 8);
+
+    }
+
 }
